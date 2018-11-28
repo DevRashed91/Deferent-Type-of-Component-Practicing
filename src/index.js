09e785f821f12d9da5f74104eee67dec13b43370
Rashadul Islam <irashad42@gmail.com>
2018-11-28 10:52:07 +0600

    update
    give some handler example into component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function Home(props) {
+function Home({ p, alertHandler }) {
   return (
     <div className="Home">
-      <p>Home: {props.p}</p>
+      <p onClick={e => alertHandler(e.target.innerText)}>Home: {p}</p>
+      <input
+        type="text"
+        name="input text"
+        placeholder="enter you value"
+        onChange={e => alertHandler(e.target.value)}
+      />
       <hr />
     </div>
   );
@@ -22,7 +28,7 @@ const Dream = ({ p }) => {
 
 const Users = ({ users, indexHandler }) => {
   return (
-    <div>
+    <div className="card users">
       {users.map(user => {
         return (
           <div className="d-flex">
@@ -51,11 +57,16 @@ class App extends React.Component {
     let myname = this.state.users[name].name;
     alert("my Name is: " + myname + " | AND My ID is: " + id);
   };
+
+  alertHandler(name) {
+    console.log(name);
+    // alert('hello, i am' + e.target.value)
+  }
   render() {
     return (
       <div>
         <p style={{ backgroundColor: "blue", color: "#fff" }}>header</p>
-        <Home p={this.state.p} />
+        <Home alertHandler={this.alertHandler.bind(this)} p={this.state.p} />
         <Dream p={this.state.p} />
         <Users
           users={this.state.users}
